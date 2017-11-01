1e8951e235f2b8d0416d2c392b94b76b65f34d8d
Chase Florell <chase.florell@gmail.com>
2017-11-01 13:00:27 -0700

    [CF] fixed

--- WorkingWithWebview/AboutPage.cs
+++ WorkingWithWebview/AboutPage.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Text.RegularExpressions;
-using Plugin.Messaging;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace WorkingWithWebview
 {
@@ -11,72 +8,9 @@ namespace WorkingWithWebview
     {
         public AboutPage()
         {
-            var browser = new WebView();
+            var browser = new WebView {Source = DependencyService.Get<IBaseUrl>().Get() + "/about.html"};
 
-            browser.Source = DependencyService.Get<IBaseUrl>().Get() + "/about.html";
-
-            browser.Navigating += async (s, e) =>
-            {
-                if (e.Url.StartsWith("http"))
-                {
-                    try
-                    {
-                        var uri = new Uri(e.Url);
-                        Device.OpenUri(uri);
-                    }
-                    catch (Exception)
-                    {
-                    }
-
-                    e.Cancel = true;
-                }
-                else if (e.Url.StartsWith("mailto"))
-                {
-                    try
-                    {
-                        //var uri = new Uri(e.Url);
-                        //Device.OpenUri(uri);
-                        var email = e.Url.Substring(e.Url.IndexOf(":") + 1);
-                        if (await DisplayAlert(null,
-                                              "Email:\n" + email + "?",
-                           "Yes", "No"))
-                        {
-                            var emailTask = CrossMessaging.Current.EmailMessenger;
-                            if (emailTask.CanSendEmail)
-                                emailTask.SendEmail(email, "I need help with EzWIC App", "");
-                        }
-                    }
-                    catch (Exception)
-                    {
-                    }
-
-                    e.Cancel = true;
-                }
-                else if (e.Url.StartsWith("call"))
-                {
-                    try
-                    {
-                        //var uri = new Uri(e.Url);
-                        var phoneToCall = e.Url.Substring(e.Url.IndexOf(":") + 1);
-                        if (await DisplayAlert(null, $"Call {phoneToCall}",
-  //                          String.Format("Call", 
- //                           Regex.Replace(phoneToCall, @"(\d{3})(\d{3})(\d{4})", "($1) $2-$3")),
-                                               "Yes", "No"))
-                        {
-                            var phoneCallTask = CrossMessaging.Current.PhoneDialer;
-                            if (phoneCallTask.CanMakePhoneCall)
-                                phoneCallTask.MakePhoneCall(phoneToCall);
-                        }
-
-                        //Device.OpenUri(uri);
-                    }
-                    catch (Exception)
-                    {
-                    }
-
-                    e.Cancel = true;
-                }
-            };
+            browser.Navigating += (s, e) => WebViewNavigationDelegate.OnNavigating(this, s, e);
 
             Content = browser;
         }
---
+++ WorkingWithWebview/WebViewNavigationDelegate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using Plugin.Messaging;
+using Xamarin.Forms;
+
+namespace WorkingWithWebview
+{
+    public class WebViewNavigationDelegate
+    {
+        public static async void OnNavigating(Page page, object sender, WebNavigatingEventArgs e)
+        {
+            e.Cancel = true;
+            try
+            {
+                if (e.Url.StartsWith("http") || e.Url.StartsWith("https"))
+                {
+                    var uri = new Uri(e.Url);
+                    Device.OpenUri(uri);
+                }
+                else if (e.Url.StartsWith("mailto"))
+                {
+                    var email = e.Url.Substring(e.Url.IndexOf(":") + 1);
+                    if (await page.DisplayAlert(null,
+                        "Email:\n" + email + "?",
+                        "Yes", "No"))
+                    {
+                        var emailTask = CrossMessaging.Current.EmailMessenger;
+                        if (emailTask.CanSendEmail)
+                            emailTask.SendEmail(email, "I need help with my App", "");
+                    }
+                }
+                else if (e.Url.StartsWith("call") || e.Url.StartsWith("tel"))
+                {
+                    var phoneToCall = e.Url.Substring(e.Url.IndexOf(":") + 1);
+                    if (await page.DisplayAlert(null, $"Call {phoneToCall}",
+                        "Yes", "No"))
+                    {
+                        var phoneCallTask = CrossMessaging.Current.PhoneDialer;
+                        if (phoneCallTask.CanMakePhoneCall)
+                            phoneCallTask.MakePhoneCall(phoneToCall);
+                    }
+                }
+                else if (e.Url.StartsWith("feedback"))
+                {
+                    await Application.Current.MainPage.Navigation.PushAsync(new FeedbackPage());
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                Debugger.Break();
+            }
+        }
+    }
+}
